21511c41f7e1c316abe8adfa60436a6239f07cb6
cian124 <cian12493@gmail.com>
2018-06-19 13:58:20 +0900

    Mostly done.

--- actors.py
+++ actors.py
@@ -6,28 +6,43 @@ class Creature:
         self.name = name
         self.level = level
 
+    def __repr__(self):
+        return "Creature: {} of level {}".format(self.name, self.level)
+
+    def defensive_roll(self):
+        roll = random.randint(1, 12)
+        attack = self.level * roll
+        return attack
+
 
 class Wizard(Creature):
-    def __init__(self):
-        super.__init__()
+    def __init__(self, name, level):
+        super().__init__(name, level)
 
-    def attack(self):
-        pass
+    def attack(self, enemy):
+        w_roll = self.defensive_roll()
+        e_roll = enemy.defensive_roll()
+        print('You roll {}'.format(w_roll))
+        print('The enemy {} rolls a {}'.format(enemy.name, e_roll))
+        if w_roll >= e_roll:
+            return True
+        else:
+            return False
 
 
 class SmallCreature(Creature):
-    pass
+    def __init__(self, name, level):
+        super().__init__(name, level)
 
 
 class Dragon(Creature):
-    def __init__(self, scaliness, breaths_fire):
-        super.__init__()
+    def __init__(self, name, level, scaliness, breaths_fire):
+        super().__init__(name, level)
         self.scaliness = scaliness
         self.breaths_fire = breaths_fire
-    pass
 
-
-def defensive_roll(level):
-    roll = random.randrange(1, 12)
-    attack = level * roll
-    return attack
+    def defensive_roll(self):
+        roll = super().defensive_roll()
+        fire_modifier = 5 if True else 1
+        roll = roll * (self.scaliness / 10) * fire_modifier
+        return roll
--- program.py
+++ program.py
@@ -1,3 +1,5 @@
+import random
+
 from actors import Creature, Wizard, SmallCreature, Dragon
 
 
@@ -9,14 +11,35 @@ def draw_header():
 
 
 def game_loop():
+    player = Wizard('Wizardy Boy', 50)
+    creatures = [SmallCreature('Rat', 1),
+                 Creature('Golem', 20),
+                 Dragon('Dragon (His name is Freddy)', 70, 20, True),
+                 Wizard('Evil Wizardy Boy', 200)]
+    current_enemy = random.choice(creatures)
+
     while True:
-        action = input('Do you want to [A]ttack, [L]ook around or [R]un away?').lower()
-        if action is 'a':
-            pass
-        elif action is 'l':
-            pass
-        elif action is 'r':
-            pass
+        print('You encounter a terrifying {}!'.format(current_enemy.name))
+        action = input('Do you want to [A]ttack, [L]ook around or [R]un away?')
+        action = action.lower()
+        if action == 'a':
+            result = player.attack(current_enemy)
+            if result:
+                print('You win buddy!')
+                current_enemy = random.choice(creatures)
+            else:
+                print('You dead.')
+                break
+
+        elif action == 'l':
+            print('You look around and see:')
+            for c in creatures:
+                print(c.name)
+
+        elif action == 'r':
+            print('You run away from the {}!'.format(current_enemy))
+            current_enemy = random.choice(creatures)
+
         else:
             print('Bye forever!')
             break
@@ -24,6 +47,7 @@ def game_loop():
 
 def main():
     draw_header()
+    game_loop()
 
 
 if __name__ == '__main__':
